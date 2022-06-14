aa67ada7fea7c893fcdae3388e479dc0f701a132
supervueman <supervueman@gmail.com>
2022-06-14 21:36:26 +0400

    Add packages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,11 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  {
+    name: 'Home',
+    path: '/',
+    component: Home,
+  },
 ]
 
 const router = createRouter({
