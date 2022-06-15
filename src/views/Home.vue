2216a6de6402bea6bbc1fbf88ccb9c152507f973
supervueman <supervueman@gmail.com>
2022-06-15 22:15:58 +0400

    Add input and select for filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,33 @@ import { $ref } from 'vue/macros';
 import { ref } from 'vue'
 
 const searchQuery = $ref('')
+const categoryQuery = $ref('')
+const options: AppSpace.FilterableItem[] = $ref([])
+
+const remoteSearch = () => {}
 </script>
 
 <template>
   <Page>
     <template #middle>Home</template>
     <div class="home">
-      <el-input placeholder="" />
+      <el-input placeholder="" v-model="searchQuery" />
+      <el-select
+        v-model="categoryQuery"
+        multiple
+        filterable
+        remote
+        reserve-keyword
+        placeholder="Please enter a keyword"
+        :remote-method="remoteSearch"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
     </div>
     <template #footer>Footer</template>
   </Page>
