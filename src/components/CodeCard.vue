ad00339051ac6674928c5d831356dd04ea732672
supervueman <supervueman@gmail.com>
2022-06-15 22:15:13 +0400

    Add code card component

---
+++ src/components/CodeCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+})
+</script>
+
+<template>
+  <el-card class="box-card">
+    <el-image :src="image" />
+    <h2 class="title">{{ title }}</h2>
+    <p></p>
+  </el-card>
+</template>
